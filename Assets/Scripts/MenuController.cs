50a81f17c23624859f9cea07756804c748997dd6
Toni Saario <Toni.saario@hotmail.fi>
2018-11-27 12:40:54 +0200

    added spawns and menu work for new map

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,31 +8,39 @@ public class MenuController : MonoBehaviour {
 
   private GameObject mainMenuPanel;
   private GameObject settingsPanel;
-	// Use this for initialization
+  private GameObject mapSelectPanel;
+  
 	void Start () {
 		mainMenuPanel = GameObject.Find("PanelMenu");
+    mapSelectPanel = GameObject.Find("PanelMapSelect");
     settingsPanel = GameObject.Find("PanelSettings");
     settingsPanel.SetActive(false);
+    mapSelectPanel.SetActive(false);
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
 
-  // Start game button
-  public void StartClick(){
-    StartCoroutine(LoadGameScene());
+  public void StartClickValley(){
+    StartCoroutine(LoadGameScene(1));
+  }
+  public void StartClickPit(){
+    StartCoroutine(LoadGameScene(2));
   }
 
-  IEnumerator LoadGameScene()
+  IEnumerator LoadGameScene(int sceneId)
   {
-      AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scene0");
+      AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneId);
 
       while (!asyncLoad.isDone)
       {
           yield return null;
       }
+  }
+  public void StartGameClick(){
+    mainMenuPanel.SetActive(false);
+    mapSelectPanel.SetActive(true);
   }
   public void SettingsClick(){
     mainMenuPanel.SetActive(false);
@@ -41,11 +49,17 @@ public class MenuController : MonoBehaviour {
   public void QuitClick(){
     Application.Quit();
   }
+
+  // return to main menu from mapselect
+  public void BackClickMapSelect(){
+    mainMenuPanel.SetActive(true);
+    mapSelectPanel.SetActive(false);
+  }
+
   // return to main menu from settings
-  public void BackClick(){
+  public void BackClickSettings(){
     mainMenuPanel.SetActive(true);
     settingsPanel.SetActive(false);
-
   }
 
 }
