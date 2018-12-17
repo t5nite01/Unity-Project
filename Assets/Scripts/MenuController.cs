93d42a9bc2cb1fada9f0697c7b8875f293a9410d
t5nite01 <t5nite01@students.oamk.fi>
2018-12-17 16:48:32 +0200

    Revert "Merge branch 'Tonii' into Tero"
    This reverts commit 82fdd6164c557a61d615f6e00bf3495dbfb3d08b.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -15,14 +15,12 @@ public class MenuController : MonoBehaviour {
   
 	void Start ()
     {
-    Time.timeScale = 1;
-    mainMenuPanel = GameObject.Find("PanelMenu");
-    mapSelectPanel = GameObject.Find("PanelMapSelect");
-    settingsPanel = GameObject.Find("PanelSettings");
-    highScorePanel = GameObject.Find("PanelHighscore");
-    highScoreleftPanel = GameObject.Find("PanelleftHighscore");
-    settingsPanel.SetActive(false);
-    mapSelectPanel.SetActive(false);
+        Time.timeScale = 1;
+        mainMenuPanel = GameObject.Find("PanelMenu");
+        mapSelectPanel = GameObject.Find("PanelMapSelect");
+        settingsPanel = GameObject.Find("PanelSettings");
+        settingsPanel.SetActive(false);
+        mapSelectPanel.SetActive(false);
 	}
 	
 	void Update () {
